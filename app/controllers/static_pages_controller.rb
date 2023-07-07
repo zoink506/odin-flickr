3b5f705057866a0d8e8ae2ec7213c94255dd37f5
zoink506 <benhubble03@gmail.com>
2023-07-07 13:58:49 +1000

    List 15 most recent public images on flickr when user_id is left blank

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,8 +4,14 @@ class StaticPagesController < ApplicationController
     @photo_links = []
 
     if params.has_key?(:user_id)
-      photos = flickr.photos.search(:user_id => params[:user_id])
-      @photo_links = photos.map { |ph| Flickr.url_m(ph) }
+      if params[:user_id] != ""
+        photos = flickr.photos.search(:user_id => params[:user_id])
+        @photo_links = photos.map { |ph| Flickr.url_m(ph) }
+      elsif params[:user_id] == ""
+        photos = flickr.photos.getRecent(per_page: 15)
+        p photos
+        @photo_links = photos.map { |ph| Flickr.url_m(ph) }
+      end
     end
 
   end
